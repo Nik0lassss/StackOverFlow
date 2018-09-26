b9d8859a1754a0a5f735feec8626f32e3cf4d622
Nikolay <nicholaschirkevich@gmail.com>
2018-09-27 00:04:10 +0300

    add android view model

---
+++ presentation/src/main/java/com/chirkevich/nikola/stackoverflow/ViewModelTest.java
@@ -0,0 +1,42 @@
+package com.chirkevich.nikola.stackoverflow;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import com.chirkevich.nikola.domain.buisness.synchronization.ISynchronizationInteractor;
+import com.chirkevich.nikola.domain.models.Items;
+
+import java.util.Date;
+
+import io.reactivex.Single;
+
+public class ViewModelTest extends AndroidViewModel {
+
+    private ISynchronizationInteractor synchronizationInteractor;
+
+    public ViewModelTest(@NonNull Application application, ISynchronizationInteractor synchronizationInteractor) {
+        super(application);
+        this.synchronizationInteractor = synchronizationInteractor;
+    }
+
+    public Single<Items> getItems(Integer page,
+                                  Date todate,
+                                  Date max,
+                                  Integer pagesize,
+                                  String order,
+                                  String sort,
+                                  Date fromdate,
+                                  Date min,
+                                  String site) {
+        return synchronizationInteractor.getAnswers(page,
+                todate,
+                max,
+                pagesize,
+                order,
+                sort,
+                fromdate,
+                min,
+                site);
+    }
+}
